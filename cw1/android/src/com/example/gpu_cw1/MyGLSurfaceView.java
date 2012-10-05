3060e3467689f5805a5d161fab2e797e914a5ee9
Szymon Sobczak <szymon.sobczak@gmail.com>
2012-10-05 14:12:04 +0200

    dont crash

--- cw1/android/src/com/example/gpu_cw1/MyGLSurfaceView.java
+++ cw1/android/src/com/example/gpu_cw1/MyGLSurfaceView.java
@@ -8,10 +8,10 @@ public class MyGLSurfaceView extends GLSurfaceView {
 	public MyGLSurfaceView(Context context) {
 		super(context);
 
-		// Render the view only when there is a change in the drawing data
-		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-
 		// Set the Renderer for drawing on the GLSurfaceView
 		setRenderer(new MyGL20Renderer());
+
+		// Render the view only when there is a change in the drawing data
+		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
 	}
 }
